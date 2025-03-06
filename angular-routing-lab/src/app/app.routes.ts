32986d401e0d77c5663bbd147701554a356a492b
UBSITAngagka <20235182@s.ubaguio.edu>
2025-03-06 16:01:17 +0800

    Made new branch and testing out the merge

--- angular-routing-lab/src/app/app.routes.ts
+++ angular-routing-lab/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './about/profile/profile.component';
 
 export const routes: Routes = [
     {path: '',redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent },
-    {path: 'about', component: AboutComponent },
-    {path: 'contact', component: ContactComponent }
+    {path: 'about', component: AboutComponent, children: [{ path: 'profile', component: ProfileComponent}]},
+    {path: 'contact', component: ContactComponent },
+    { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
 ];
 
 @NgModule({
